a86447c85103a32282e70c8a17231f6d1679ebcc
Federico Peregrin <1678federico@gmail.com>
2023-04-25 20:37:21 -0300

    Landing page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Clients from "./components/Clients"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Clients />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Clients.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Clients = () => {
+  return (
+    <div className="bg-gray-100 p-8 flex flex-col items-center justify-center gap-8 mt-20 xl:mt-0">
+        <h1 className="text-2xl font-medium text-gray-800 text-center">Trusted by greatest companies</h1>
+        <div className="flex flex-col md:flex-row items-center gap-20 flex-wrap">
+        <img src="google.png" className="w-40" />
+        <img src="airbnb.png" className="w-40" />
+        <img src="amazon.png" className="w-40" />
+        <img src="shopify.png" className="w-40" />
+        <img src="creative.png" className="w-40" />
+        </div>
+    </div>
+  )
+}
+
+export default Clients
