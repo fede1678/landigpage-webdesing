650fbcd08a1cb66f38e208f12689b067a9a5bc8d
Federico Peregrin <1678federico@gmail.com>
2023-04-25 22:09:24 -0300

    si

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -4,7 +4,7 @@ const Clients = () => {
   return (
     <div className="bg-gray-100 p-8 flex flex-col items-center justify-center gap-8 mt-20 xl:mt-0">
         <h1 className="text-2xl font-medium text-gray-800 text-center">Trusted by greatest companies</h1>
-        <div className="flex flex-col md:flex-row items-center gap-20 flex-wraps">
+        <div className="flex flex-col md:flex-row items-center gap-20 flex-wrap">
         <img src="google.png" className="w-40" />
         <img src="airbnb.png" className="w-40" />
         <img src="amazon.png" className="w-40" />
